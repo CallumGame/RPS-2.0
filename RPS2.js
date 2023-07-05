32feacb1066fdd4da70851387e40d31fa4dc1e66
CallumGame <callumutd@live.co.uk>
2023-07-05 14:34:54 +0300

    added the appendChild to make the p element appear in the html

--- RPS2.js
+++ RPS2.js
@@ -21,40 +21,44 @@ const playRound = (playerSelection, computerSelection) =>{
     if (playerSelection ===  computerSelection ){
         const p = document.createElement('p')
        p.innerText = `You draw! You both picked ${playerSelection} , play again`
+       result.appendChild(p)
     } 
 else if (playerSelection === "rock" && computerSelection === "scissors"){
     const p = document.createElement('p')
     p.innerText = "You win! The computer picked Scissors"
     playerScore++
+    result.appendChild(p)
 }
 else if (playerSelection === "paper" && computerSelection === "rock"){
     const p = document.createElement('p')
     p.innerText = "You win! The computer picked Rock"
     playerScore++
+    result.appendChild(p)
 }
 else if (playerSelection === "scissors" && computerSelection === "paper"){
     const p = document.createElement('p')
     p.innerText = "You win! The computer picked Paper"
     playerScore++
+    result.appendChild(p)
 
 }
 else if (playerSelection === "rock" && computerSelection === "paper"){
     const p = document.createElement('p')
     p.innerText = "You lose! The computer picked Paper"
     computerScore++
-
+    result.appendChild(p)
 }
 else if (playerSelection === "paper" && computerSelection === "scissors"){
     const p = document.createElement('p')
     p.innerText = "You lose! The computer picked Scissors"
     computerScore++
-
+    result.appendChild(p)
 }
 else if (playerSelection === "scissors" && computerSelection === "rock"){
     const p = document.createElement('p')
     p.innerText =  "You lose! The computer picked Rock"
     computerScore++
-
+    result.appendChild(p)
 }
 
 }
