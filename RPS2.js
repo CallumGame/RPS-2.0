a01f5e57a7a8fff3e977a8e012455f4331f06876
CallumGame <callumutd@live.co.uk>
2023-07-05 15:39:26 +0300

    created a function to get the outcome of the result

--- RPS2.js
+++ RPS2.js
@@ -6,6 +6,7 @@ const rockButton = document.querySelector('#rock')
 const scissorButton = document.querySelector('#scissor')
 const paperButton = document.querySelector('#paper')
 const result = document.querySelector('#result')
+const container = document.querySelector('#container');
 
 const getComputerChoice = () => {
 
@@ -17,7 +18,6 @@ return choices
 
 const playRound = (playerSelection, computerSelection) =>{
 
-    console.log('1 ', playerSelection, '2 ', computerSelection)
     if (playerSelection ===  computerSelection ){
         const p = document.createElement('p')
        p.innerText = `You draw! You both picked ${playerSelection} , play again`
@@ -63,22 +63,39 @@ else if (playerSelection === "scissors" && computerSelection === "rock"){
 
 }
 
+const winner = (playerScore, computerScore) => {
+if (playerScore === 5){
+    const h3 = document.createElement('h3')
+    h3.classList.add('PlayerWin')
+    h3.innerText = `Congratulations you win! Nothing, you beat the computer by ${playerScore} to ${computerScore}.`
+} 
+else if (computerScore === 5){
+    const h3 = document.createElement('h3')
+    h3.classList.add('PlayerWin')
+    h3.innerText = `HJFHJDH you lose!, you were beaten by the computer by ${computerScore} to ${playerScore}.`
+}
+}
+
+
 rockButton.addEventListener('click', () => {
 const computerSelection = getComputerChoice()
 const playerSelection = 'rock'
 playRound(playerSelection, computerSelection)
+winner(playerScore, computerScore)
 })
 
 paperButton.addEventListener('click', () => {
 const computerSelection = getComputerChoice()
 const playerSelection = "paper"
 playRound(playerSelection, computerSelection)
+winner(playerScore, computerScore)
 })
 
 scissorButton.addEventListener('click', () => {
 const computerSelection = getComputerChoice()
 const playerSelection = "scissor" 
 playRound(playerSelection, computerSelection)
+winner(playerScore, computerScore)
 })
 
 
